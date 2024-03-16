4a66ddf01b0d514a7464b896afda31762bd3608c
Vadim <cybersnuff@yandex.ru>
2024-03-16 13:07:10 +0400

    commit

--- src/main/java/com/crud/boot/resttemplate/RestTemplateApplication.java
+++ src/main/java/com/crud/boot/resttemplate/RestTemplateApplication.java
@@ -11,11 +11,13 @@ import java.util.List;
 public class RestTemplateApplication {
     static final String URL = "http://94.198.50.185:7081/api/users";
 
-
     public static void main(String[] args) {
 
         StringBuilder result = new StringBuilder();
         RestTemplate restTemplate = new RestTemplate();
+        User saveUser = new User(3L, "James", "Brown", (byte) 29);
+        User updatedUser = new User(3L, "Thomas", "Shelby", (byte) 29);
+        String deleteUrl = URL + "/3";
 
         ResponseEntity<String> responseEntity =
                 restTemplate.getForEntity(URL, String.class);
@@ -41,20 +43,17 @@ public class RestTemplateApplication {
         headers.add("Cookie", sessionId); // Установка заголовка Cookie с session id
         headers.setContentType(MediaType.APPLICATION_JSON);
 
-        User saveUser = new User(3L, "James", "Brown", (byte) 29);
         // попытка создания нового юзера
         HttpEntity<User> save = new HttpEntity<>(saveUser, headers);
         ResponseEntity<String> responseEntity1 = restTemplate.exchange(URL, HttpMethod.POST, save, String.class);
         result.append(responseEntity1.getBody());
 
         //попытка апдейта
-        User updatedUser = new User(3L, "Thomas", "Shelby", (byte) 29);
         HttpEntity<User> update = new HttpEntity<>(updatedUser, headers);
         ResponseEntity<String> responseEntity2 = restTemplate.exchange(URL, HttpMethod.PUT, update, String.class);
         result.append(responseEntity2.getBody());
 
         //попытка удаления
-        String deleteUrl = URL + "/3";
         HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
         ResponseEntity<String> responseEntity3 = restTemplate.exchange(deleteUrl, HttpMethod.DELETE, requestEntity, String.class);
         result.append(responseEntity2.getBody());
