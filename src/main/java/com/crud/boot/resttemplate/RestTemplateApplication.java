05b15f41153081df58a696d56f0cab4c28e73fd3
Vadim <cybersnuff@yandex.ru>
2024-03-21 22:25:24 +0400

    commit

--- src/main/java/com/crud/boot/resttemplate/RestTemplateApplication.java
+++ src/main/java/com/crud/boot/resttemplate/RestTemplateApplication.java
@@ -5,58 +5,58 @@ import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.Objects;
 
 
 public class RestTemplateApplication {
     static final String URL = "http://94.198.50.185:7081/api/users";
+    static StringBuilder result = new StringBuilder();
+    static RestTemplate restTemplate = new RestTemplate();
+    static User saveUser = new User(3L, "James", "Brown", (byte) 29);
+    static User updatedUser = new User(3L, "Thomas", "Shelby", (byte) 29);
+    static String deleteUrl = URL + "/3";
+    static HttpHeaders headers = new HttpHeaders();
 
-    public static void main(String[] args) {
-
-        StringBuilder result = new StringBuilder();
-        RestTemplate restTemplate = new RestTemplate();
-        User saveUser = new User(3L, "James", "Brown", (byte) 29);
-        User updatedUser = new User(3L, "Thomas", "Shelby", (byte) 29);
-        String deleteUrl = URL + "/3";
-
-        ResponseEntity<String> responseEntity = restTemplate.getForEntity(URL, String.class);
-        System.out.println("Список всех пользователей: " + responseEntity.getBody());
-
-        List<String> setCookieHeaders = responseEntity.getHeaders().get(HttpHeaders.SET_COOKIE);
-
-        //вытащил свой session id
-        String sessionId = null;
-        if (setCookieHeaders != null) {
-            for (String setCookieHeader : setCookieHeaders) {
-                if (setCookieHeader.contains("SESSION")) {
-                    sessionId = setCookieHeader.split(";")[0]; // Выбираем только значение до первой точки с запятой
-                    break;
-                }
-            }
-        }
-        System.out.println("Сессия: " + sessionId);
-
-        String session = sessionId;
-        System.out.println(session);
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Cookie", sessionId); // Установка заголовка Cookie с session id
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
-        // попытка создания нового юзера
-        HttpEntity<User> save = new HttpEntity<>(saveUser, headers);
+    public static String save(HttpHeaders headers, User userSave) {
+        HttpEntity<User> save = new HttpEntity<>(userSave, headers);
         ResponseEntity<String> responseEntitySave = restTemplate.exchange(URL, HttpMethod.POST, save, String.class);
-        result.append(responseEntitySave.getBody());
+        return responseEntitySave.getBody();
+    }
 
-        //попытка апдейта
-        HttpEntity<User> update = new HttpEntity<>(updatedUser, headers);
+    public static String update(HttpHeaders headers, User userUpdate) {
+        HttpEntity<User> update = new HttpEntity<>(userUpdate, headers);
         ResponseEntity<String> responseEntityUpdate = restTemplate.exchange(URL, HttpMethod.PUT, update, String.class);
-        result.append(responseEntityUpdate.getBody());
+        return responseEntityUpdate.getBody();
+    }
 
-        //попытка удаления
+    public static String delete(HttpHeaders headers, User userUpdate) {
         HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
         ResponseEntity<String> responseEntityDelete = restTemplate.exchange(deleteUrl, HttpMethod.DELETE, requestEntity, String.class);
-        result.append(responseEntityDelete.getBody());
+        return responseEntityDelete.getBody();
 
+    }
+
+    public static String getAllUsers() {
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(URL, String.class);
+        return String.join(";", Objects.requireNonNull(responseEntity.getHeaders().get("set-cookie")));
+    }
+
+    public RestTemplateApplication() {
+        String sessionId = getAllUsers();
+        headers.set("cookie", sessionId);
+    }
+
+    public static void main(String[] args) {
+        RestTemplateApplication restTemplateApplication = new RestTemplateApplication();
+        result
+                .append(save(headers, saveUser))
+                .append(update(headers, updatedUser))
+                .append(delete(headers, updatedUser));
         System.out.println("Итоговый код \"" + result + "\" - состоит из " + result.length() + " символов");
     }
 }
+
+
+
+
+
